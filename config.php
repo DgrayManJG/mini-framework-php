4cd1af7e018845a4d82e06767f68b51654c95ae3
DgrayManJG <allenwalkerj@gmail.com>
2017-04-28 19:02:58 +0200

    file name modified

--- config.php
+++ config.php
@@ -4,4 +4,4 @@
 define("DBHOST", "localhost");  //soit localhost, soit l'IP du serveur
 define("DBUSER", "root");       //utilisateur de la base (différent de PHPmyAdmin)
 define("DBPASS", "");           //mot de passe
-define("DBNAME", "dico");  	    //nom de la base de données
+define("DBNAME", "wiki");  	    //nom de la base de données
